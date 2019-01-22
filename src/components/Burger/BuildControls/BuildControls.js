24e43dcda3efa60a6e5e778e8678bea49d490c04
suttonau <sutton_mjbc@icloud.com>
2019-01-22 17:34:56 -0500

    remove ingredient handler, and disable less button if ingredients === 0

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,13 +11,15 @@ const controls = [
 
 const BuildControls = props => (
   <div className={classes.BuildControls}>
-    {controls.map(ctrl => {
+    {controls.map(ctrl => (
       <BuildControl
         key={ctrl.label}
         label={ctrl.label}
         added={() => props.ingredientAdded(ctrl.type)}
-      />;
-    })}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+      />
+    ))}
   </div>
 );
 export default BuildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -46,13 +46,47 @@ class BurgerBuilder extends Component {
     });
   };
 
-  removeIngredientHandler = type => {};
+  removeIngredientHandler = type => {
+    const origCount = this.state.ingredients[type];
+    //condition for negative ingredients, if negative return nothing
+    if (origCount <= 0) {
+      return;
+    }
+    const updatedCount = origCount - 1;
 
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const origPrice = this.state.totalPrice;
+    const newPrice = origPrice - priceDeduction;
+
+    this.setState({
+      totalPrice: newPrice,
+      ingredients: updatedIngredients
+    });
+  };
   render() {
+    //disabling the less button if ingredients are 0
+    const disabledIng = {
+      ...this.state.ingredients
+    };
+    //setting values to true or false if ingredients are at 0
+    for (let key in disabledIng) {
+      disabledIng[key] = disabledIng[key] <= 0;
+    }
+    // {salad: true, meat: false, etc...}
+
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls ingredientAdded={this.addIngredientHandler} />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledIng}
+        />
       </Aux>
     );
   }
