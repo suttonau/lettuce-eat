2150766029032df71ae63e56eaeaf3db9c65a3b1
suttonau <sutton_mjbc@icloud.com>
2019-01-30 19:33:48 -0500

    completed logo, messing around with styling on navbar

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,11 @@
 import React from "react";
+import logo from "../../assets/images/lettuce-eat-logo.png";
+import classes from "./Logo.css";
 
-const Logo = props => {
-  <div>Created LOGO</div>;
-};
+const Logo = props => (
+  <div className={classes.Logo}>
+    <img src={logo} alt="MyLogo" />
+  </div>
+);
 
 export default Logo;
