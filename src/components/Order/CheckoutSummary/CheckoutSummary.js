4281fc494b6acd99bbd3ebd19f8a7ccb5ebae238
suttonau <sutton_mjbc@icloud.com>
2019-02-10 22:22:55 -0500

    Setting up cancel and continue buttons on checkout

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -10,10 +10,10 @@ const CheckoutSummary = props => {
       <div style={{ width: "100%", margin: "auto" }}>
         <Burger ingredients={props.ingredients} />
       </div>
-      <Button btnType="Danger" clicked>
+      <Button btnType="Danger" clicked={props.onCheckoutCancelled}>
         Cancel
       </Button>
-      <Button btnType="Success" clicked>
+      <Button btnType="Success" clicked={props.onCheckoutContinued}>
         Continue
       </Button>
     </div>
